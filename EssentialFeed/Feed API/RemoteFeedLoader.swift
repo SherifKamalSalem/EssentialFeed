388354003f6c4a1f17dd17f9f54dcb6ecee49f40
Sherif Kamal <s.kamal@aqwas.sa>
2021-06-04 22:53:27 +0200

    Remove the generic error type from the 'LoadFeedResult' to identify type constraints

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -10,7 +10,7 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
     private let url: URL
     private let client: HTTPClient
-    public typealias Result = LoadFeedResult<Error>
+    public typealias Result = LoadFeedResult
     
     public enum Error: Swift.Error {
         case connectivity
@@ -29,7 +29,7 @@ public final class RemoteFeedLoader: FeedLoader {
             case let .success(data, response):
                 completion(FeedItemsMapper.map(data, from: response))
             case .failure:
-                completion(.failure(.connectivity))
+                completion(.failure(Error.connectivity))
             }
         }
     }
