f75e2fbae04d988501d6369f64c0fed6573505a7
Sherif Kamal <s.kamal@aqwas.sa>
2021-06-15 18:33:26 +0200

    Move intercepting start/stop methods to setup and tearDown methods

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -25,7 +25,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             self.session = session
         }
         
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)   {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             session.dataTask(with: url) { _, _, error in
                 if let error = error {
                     completion(.failure(error))
@@ -34,10 +34,20 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
     }
     
+    override class func setUp() {
+        super.setUp()
+        URLProtocolStub.startInterceptingRequests()
+        
+    }
+    
+    override class func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
     func test_getFromURL_performGETRequestWithURL() {
         let url = URL(string: "http://any-url.com")!
         let exp = expectation(description: "Wait for request")
-        URLProtocolStub.startInterceptingRequests()
         URLProtocolStub.observeRequests { request in
             XCTAssertEqual(request.url, url)
             XCTAssertEqual(request.httpMethod, "GET")
@@ -45,11 +55,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         URLSessionHTTPClient().get(from: url) { _ in }
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     func test_getFromURL_failsOnRequestError() {
-        URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
@@ -67,7 +75,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     //MARK: - Helpers
