6a1d4d0ce19afef88f1b0af1c90caf3eda9bb45b
Sherif Kamal <s.kamal@aqwas.sa>
2021-06-18 03:00:54 +0200

    Enable code coverage

--- EssentialFeed/FeedFeature/FeedItemsMapper.swift
+++ EssentialFeed/FeedFeature/FeedItemsMapper.swift
@@ -31,7 +31,7 @@ internal final class FeedItemsMapper {
     private static var ok_200: Int { return 200 }
     
     static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-        guard response.statusCode == 200,
+        guard response.statusCode == ok_200,
               let root = try? JSONDecoder().decode(Root.self, from: data) else {
             return .failure(RemoteFeedLoader.Error.invalidData)
         }
