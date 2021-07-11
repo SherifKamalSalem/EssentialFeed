46db29bde0c4eaaa9dc3369fa58d81ab1002a5f1
Sherif Kamal <sherif.kamal.salem@gmail.com>
2021-07-11 23:57:25 +0200

    Extract UIImage test helper to a separate file

---
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIImage+TestHelpers.swift
@@ -0,0 +1,9 @@
+//
+//  UIImage+WithColorExt.swift
+//  EssentialFeediOSTests
+//
+//  Created by sherif kamal on 11/07/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
