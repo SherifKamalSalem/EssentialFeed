2eda94b14e205b6120b1087639dd2471552b9555
Sherif Kamal <sherif.kamal.salem@gmail.com>
2021-07-13 19:24:37 +0200

    Decouple FeedImageViewModel from UIKit by creating a transformation closure that converts an image `Data` value into a generic `Image` type.
     When composing with a UIKit user interface, we inject a closure to transform the image `Data` into `UIImage`.

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedImageViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  FeedImageViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by sherif kamal on 13/07/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import EssentialFeed
+
+public class FeedImageViewModel<Image> {
+    typealias Observer<T> = (T) -> Void
+    
+    private var task: FeedImageDataLoaderTask?
+    private let imageLoader: FeedImageDataLoader
+    private let model: FeedImage
+    private let imageTransformer: (Data) -> Image?
+    
+    init(model: FeedImage, imageLoader: FeedImageDataLoader, imageTransformer: @escaping (Data) -> Image?) {
+        self.model = model
+        self.imageLoader = imageLoader
+        self.imageTransformer = imageTransformer
+    }
+    
+    var description: String? {
+        model.description
+    }
+    
+    var location: String? {
+        model.location
+    }
+    
+    var hasLocation: Bool {
+        location != nil
+    }
+    
+    var onImageLoad: Observer<Image>?
+    var onImageLoadingStateChange: Observer<Bool>?
+    var onShouldRetryImageLoadStateChange: Observer<Bool>?
+    
+    func loadImageData() {
+        onImageLoadingStateChange?(true)
+        onShouldRetryImageLoadStateChange?(false)
+        task = imageLoader.loadImageData(from: model.url) { [weak self] result in
+            self?.handle(result)
+        }
+    }
+    
+    private func handle(_ result: FeedImageDataLoader.Result) {
+        if let image = (try? result.get()).flatMap(imageTransformer) {
+            onImageLoad?(image)
+        } else {
+            onShouldRetryImageLoadStateChange?(true)
+        }
+        onImageLoadingStateChange?(false)
+    }
+    
+    func cancelImageDataLoad() {
+        task?.cancel()
+        task = nil
+    }
+}
+
