94b9593fbb7e581c1f9f6e0c0d53024532ba5547
Sherif Kamal <s.kamal@aqwas.sa>
2021-07-16 03:38:14 +0200

    Move `FeedLoadingViewModel` into it's own file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by sherif kamal on 16/07/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
