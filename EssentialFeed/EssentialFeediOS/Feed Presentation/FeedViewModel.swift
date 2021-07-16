57dab86c4dd2b43826821b0679e4673a8395e279
Sherif Kamal <s.kamal@aqwas.sa>
2021-07-16 03:37:49 +0200

    Move `FeedViewModel` into it's own file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by sherif kamal on 16/07/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
