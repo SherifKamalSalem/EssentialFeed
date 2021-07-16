423031c5fe4a03cb647aa76f178773982ddb7817
Sherif Kamal <s.kamal@aqwas.sa>
2021-07-16 03:51:42 +0200

    Rename FeedViewControllerTests to FeedUIIntegrationTests since we are testing the composition of multiple UI components in integration

--- EssentialFeed/EssentialFeediOSTests/Helpers/FeedViewControllerTests+Localizations.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/FeedViewControllerTests+Localizations.swift
@@ -10,7 +10,7 @@ import EssentialFeediOS
 import UIKit
 import XCTest
 
-extension FeedViewControllerTests {
+extension FeedUIIntegrationTests {
     func localized(_ key :String, file: StaticString = #file, line: UInt = #line) -> String {
         let table = "Feed"
         let bundle = Bundle(for: FeedViewController.self)
