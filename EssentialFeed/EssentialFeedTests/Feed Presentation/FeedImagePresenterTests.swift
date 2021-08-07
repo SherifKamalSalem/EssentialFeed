b09cc59db3b08fb20bb096e9eb6564024726d9cb
Sherif Kamal <sherif.kamal.salem@gmail.com>
2021-08-07 18:18:33 +0200

    Move FeedImagePresenter and collaborators to production

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -10,63 +10,6 @@ import XCTest
 import EssentialFeed
 import UIKit
 
-protocol FeedImageView {
-    associatedtype Image
-    
-    func display(_ model: FeedImageViewModel<Image>)
-}
-
-struct FeedImageViewModel<Image> {
-    let description: String?
-    let location: String?
-    let image: Image?
-    let isLoading: Bool
-    let shouldRetry: Bool
-    
-    var hasLocation: Bool {
-        return location != nil
-    }
-}
-
-class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
-    private let view: View
-    private let imageTransformer: (Data) -> Image?
-    
-    init(view: View, imageTransformer: @escaping (Data) -> Image?) {
-        self.view = view
-        self.imageTransformer = imageTransformer
-    }
-    
-    private struct InvalidImageDataError: Error {}
-    
-    func didStartLoadingImageData(for model: FeedImage) {
-        view.display(FeedImageViewModel<Image>(
-                        description: model.description,
-                        location: model.location,
-                        image: nil,
-                        isLoading: true,
-                        shouldRetry: false))
-    }
-    
-    func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
-        let image = imageTransformer(data)
-        view.display(FeedImageViewModel(
-                        description: model.description,
-                        location: model.location,
-                        image: image,
-                        isLoading: false,
-                        shouldRetry: image == nil))
-    }
-    
-    func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
-        view.display(FeedImageViewModel<Image>(
-                        description: model.description,
-                        location: model.location,
-                        image: nil,
-                        isLoading: false,
-                        shouldRetry: true))
-    }
-}
 
 class FeedImagePresenterTests: XCTestCase {
 
